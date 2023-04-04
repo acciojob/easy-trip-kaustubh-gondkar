2c60139ee03f31e56290e5a4ab28d5e19f181d0f
abc <abc@gamil.com>
2023-04-04 12:29:35 +0530

    Second

--- src/main/java/com/driver/controllers/AirportController.java
+++ src/main/java/com/driver/controllers/AirportController.java
@@ -101,7 +101,7 @@ public class AirportController {
         //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
         //This will not include the current person who is trying to book, he might also be just checking price
        int peoples = flightToPassengerDb.get(flightId).size();
-       return 3000 + peoples + 50 ;
+       return 3000 + peoples * 50 ;
     }
 
 
